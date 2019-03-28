d6e2f03eddb6647999a207647ae709168e57de86
starbuck <mausuf@utexas.edu>
2019-03-28 17:01:39 -0500

    updated html files, initial survey setup

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,11 +1,12 @@
 // ===============================================================================
 // DATA
+// Data to hold array of the coolest friends to get matched with!
 // ===============================================================================
 
 var friendsArray = [
     {
       name: "Mr. Cain",
-      photo: "decardcain@gmail.com",
+      photo: "http://heroes.blizzplanet.com/wp-content/uploads/2018/04/deckard-cain-transparent.png",
       score: [
         5,1,4,4,5,1,2,5,4,1
       ]
