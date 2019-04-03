cbb6c2e3f8fa22def621f85d0a565a04f21b67b0
starbuck <mausuf@utexas.edu>
2019-04-03 17:14:51 -0500

    fixed modal error, modal now pops up but unpopulated

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,7 +4,7 @@
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
 var path = require('path');
-var friends = require("../data/friends.js");
+var friends = require("../data/friends");
 
 // ===============================================================================
 // ROUTING
