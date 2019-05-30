a988a8df0f7a7596404ba78032377889ab3d242e
starbuck <mausuf@utexas.edu>
2019-05-30 18:55:11 -0500

    heroku deployment update

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // DEPENDENCIES
 // Series of npm packages that we will use to give our server useful functionality
 // ==============================================================================
+require("dotenv").config();
 
 var express = require("express");
 var bodyParser = require("body-parser");
